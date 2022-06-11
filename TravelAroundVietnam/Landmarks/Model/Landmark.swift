a4b4173d9d9fb6c04b95c2b661ee896acc725f31
Am-hub-dev <82162492+Am-hub-dev@users.noreply.github.com>
2022-06-11 20:48:37 +0700

    Add files via upload

---
+++ TravelAroundVietnam/Landmarks/Model/Landmark.swift
@@ -0,0 +1,37 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A representation of a single landmark.
+*/
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+    var isFeatured: Bool
+    var link: String
+
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case city = "City"
+        case historicallandmark = "Historical Landmark"
+        case naturelandmark = "Nature Landmark"
+        case landmark = "Landmark"
+    }
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+}
---
+++ TravelAroundVietnam/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
+    var body: some View {
+        ScrollView {
+
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+                Spacer()
+                Text("To see more detail, go: \(landmark.link)")
+                Spacer()
+                landmark.image
+                    .resizable()
+                    .frame(width: 400, height: 293)
+                Text("      Image by Wikipedia")
+                    .italic()
+                    .font(.footnote)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+
+    static var previews: some View {
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
+}
